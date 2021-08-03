647fe7ec61d1e5392bbf74220f65cf5fd2017893
Dinh Nam Nguyen <nguyennamnade22@gmail.com>
2021-08-03 17:36:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,20 +24,24 @@ function hidemenu(){
 }
 function showsub(){
 	if(submenu == false){
+		document.getElementById('socials1').innerHTML = 'Socials & Links <i class="fas fa-angle-up"></i>';
 		document.getElementById('submenu').style.display = 'block';
 		submenu = true;
 	}
 	else if(submenu == true){
+		document.getElementById('socials1').innerHTML = 'Socials & Links <i class="fas fa-angle-down"></i>';
 		document.getElementById('submenu').style.display = 'none';
 		submenu = false;
 	}
 }
 function showsubnav(){
 	if(subnav == false){
+		document.getElementById('socials').innerHTML = 'Socials & Links <i class="fas fa-angle-up"></i>';
 		document.getElementById('subnav').style.display = 'block';
 		subnav = true;
 	}
 	else if(subnav == true){
+		document.getElementById('socials').innerHTML = 'Socials & Links <i class="fas fa-angle-down"></i>';
 		document.getElementById('subnav').style.display = 'none';
 		subnav = false;
 	}
