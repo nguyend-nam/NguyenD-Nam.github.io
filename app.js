cd6ebd28bad4b61c76de69a99aa853901f63cf8c
Dinh Nam Nguyen <nguyennamnade22@gmail.com>
2021-08-11 13:28:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -46,3 +46,16 @@ function showsubnav(){
 		subnav = false;
 	}
 }
+let processScroll = () => {
+	let docElem = document.documentElement, 
+		docBody = document.body,
+		scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+    	scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+		scrollPercent = scrollTop / scrollBottom * 100 + '%';
+	
+	// console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
+	
+    document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent);	
+}
+
+document.addEventListener('scroll', processScroll);
