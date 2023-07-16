11be089705da451c2c0d6500df1f79d6016afaea
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-07-16 23:04:11 +0700

    fix: do not toggle sidebar when scrolling vertically

--- components/SwipeWrapper/SwipeWrapper.jsx
+++ components/SwipeWrapper/SwipeWrapper.jsx
@@ -6,53 +6,74 @@ export const SwipeWrapper = ({ children }) => {
   const setSideBar = (sideBar) => contextValue.toggleSideBar(sideBar);
 
   const [touchStart, setTouchStart] = useState(null);
+  const [touchStartVertical, setTouchStartVertical] = useState(null);
 
   // the required distance between touchStart and touchEnd to be detected as a swipe
   const minSwipeDistanceMobile = 35;
   const minSwipeDistanceDesktop = 100;
+  const maxSwipeDistanceVerticalMobile = 25;
+  const maxSwipeDistanceVerticalDesktop = 75;
 
   const onTouchStart = (e) => {
     setTouchStart(e.targetTouches[0].clientX);
+    setTouchStartVertical(e.targetTouches[0].clientY);
   };
 
   const onMouseDown = (e) => {
     setTouchStart(e.clientX);
+    setTouchStartVertical(e.clientY);
   };
 
   const onTouchMove = (e) => {
-    if (e.targetTouches[0].clientX && touchStart) {
+    if (
+      e.targetTouches[0].clientX &&
+      e.targetTouches[0].clientY &&
+      touchStart &&
+      touchStartVertical
+    ) {
       if (
-        e.targetTouches[0].clientX > touchStart &&
-        e.targetTouches[0].clientX - touchStart > minSwipeDistanceMobile
+        Math.abs(e.targetTouches[0].clientY - touchStartVertical) <
+        maxSwipeDistanceVerticalMobile
       ) {
-        setSideBar(true);
-      } else if (
-        e.targetTouches[0].clientX < touchStart &&
-        touchStart - e.targetTouches[0].clientX > minSwipeDistanceMobile
-      ) {
-        setSideBar(false);
+        if (
+          e.targetTouches[0].clientX > touchStart &&
+          e.targetTouches[0].clientX - touchStart > minSwipeDistanceMobile
+        ) {
+          setSideBar(true);
+        } else if (
+          e.targetTouches[0].clientX < touchStart &&
+          touchStart - e.targetTouches[0].clientX > minSwipeDistanceMobile
+        ) {
+          setSideBar(false);
+        }
       }
     }
   };
 
   const onMouseMove = (e) => {
-    if (e.clientX && touchStart) {
+    if (e.clientX && e.clientY && touchStart && touchStartVertical) {
       if (
-        e.clientX > touchStart &&
-        e.clientX - touchStart > minSwipeDistanceDesktop
+        Math.abs(e.clientY - touchStartVertical) <
+        maxSwipeDistanceVerticalDesktop
       ) {
-        setSideBar(true);
-      } else if (
-        e.clientX < touchStart &&
-        touchStart - e.clientX > minSwipeDistanceDesktop
-      ) {
-        setSideBar(false);
+        if (
+          e.clientX > touchStart &&
+          e.clientX - touchStart > minSwipeDistanceDesktop
+        ) {
+          setSideBar(true);
+        } else if (
+          e.clientX < touchStart &&
+          touchStart - e.clientX > minSwipeDistanceDesktop
+        ) {
+          setSideBar(false);
+        }
       }
     }
   };
 
   const onTouchOrMouseEnd = () => {
     setTouchStart(null);
+    setTouchStartVertical(null);
   };
 
   return (
